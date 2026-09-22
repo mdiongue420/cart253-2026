298dfc0e981e1d145a8886c05c562d9d6c3ec36a
mdiongue420 <m_diongu@live.concordia.ca>
2026-09-22 13:21:24 -0500

    added new files

---
+++ version-control-workflow (1)/template-p5-project/js/script.js
@@ -0,0 +1,40 @@
+/**
+ * class one
+ * Mai
+ * 
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    //single line comment
+createCanvas(400, 400);
+background(0,0,0);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(0,20,50);
+    fill(200,200,255);
+    stroke(300,90,30);
+    ellipse(300,100,25,20);
+    ellipse(300,150,25,20);
+
+
+    //rect(20,50,50,50);
+    //rect(20,50,150,150);
+    ellipse(20,60,150,150);
+    ellipse(20,70,150,150);
+    ellipse(20,80,150,150);
+    
+    fill(120,70,200);
+    ellipse(50,90,150,150);
+    circle(50,100,150)
+    arc(50,30,85,75);
+}
