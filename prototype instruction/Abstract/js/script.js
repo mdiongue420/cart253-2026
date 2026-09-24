83c663ca4dec82bdc9d3ca9c4bc3a18bdb8e3c95
mdiongue420 <m_diongu@live.concordia.ca>
2026-09-23 19:46:05 -0500

    created abstract

--- prototype instruction/Abstract/js/script.js
+++ prototype instruction/Abstract/js/script.js
@@ -19,13 +19,22 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-    background(0,0,0)
+    background(135,206,235);
     fill (255,69,0)
-    triangle(200,200, 150,150, 250,150 );
-    fill (	255,255,255)
-    ellipse(80,80,50,50);
-    fill (	255,255,255)
-    ellipse(300,80,50,50);
+    ellipse(80, 50, 80, 80);
+     fill (107,142,35)
+    ellipse(300, 400, 80, 80);
+    triangle(200, 200, 150, 150, 250, 150);
 
+    stroke('magenta');
+    strokeWeight(5);
+    line(30, 20, 85, 75);
+    
+    fill(106,90,205);
+    quad(50, 62, 86, 50, 50, 38, 14, 50);
+    fill(106,90,205);
+    quad(100,100, 200, 200, 300, 300, 250 ,250);
 
+    
+    
 }
