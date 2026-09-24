991a833511fffd8e7261ab3687f988ccef9b5abd
mdiongue420 <m_diongu@live.concordia.ca>
2026-09-23 19:31:04 -0500

    created prototype instruction
    added penguin, abstract and weird prototype

---
+++ prototype instruction/Penguin/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Prototype Instruction #1
+ * Maimouna 
+ * 
+ * a cat
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(400, 400);
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(0,0,0)
+    fill (255,69,0)
+    triangle(200,200, 150,150, 250,150 );
+    fill (	255,255,255)
+    ellipse(80,80,50,50);
+    fill (	255,255,255)
+    ellipse(300,80,50,50);
+
+
+}
--- variables-challenge/variable/js/script copy.js
+++ variables-challenge/variable/js/script copy.js
@@ -11,6 +11,36 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
  * This function is called once at the beginning of the program to initialize the canvas and any variables.
  */
+//let ellipse_size = 50;
+//let rect_size = 50;
+//let sunX = 20;
+//let sunY = 50;
+let sunSize = 50;
+let r = 255;
+let g = 255;
+let b = 0;
+
+let BackGroundColor = 0;
+
+let sun = { 
+    sunX: 20,
+    sunY: 50,
+    sunSize: 50,
+    r: 255,
+    g: 255,
+    b: 0
+    } 
+    
+
+let bird = {
+    birdX: 20,
+    birdY: 50,
+    birdSize: 50,
+    r: 0,
+    g: 0,
+    b: 255   
+
+}   
 function setup() {
      createCanvas(400, 400);
 
@@ -24,10 +54,17 @@ function setup() {
 function draw() {
     background(0,0,0)
     fill (255, 0,0); 
-    ellipse(250,250,ellipse_size, ellipse_size);
-    fill(0,0,255);
-    rect(width/2, height/2,rect_size);
-    ellipse_size = ellipse_size-1;
-    rect_size=rect_size+1;
+    ellipse (sun.sunX, sun.sunY, sun.sunSize, sun.sunSize);
+    
+    fill(bird.r, bird.g, bird.b);
+    ellipse(bird.birdX, bird.birdY, bird.birdSize, bird.birdSize);
+    bird.birdX = bird.birdX + 1;
+    bird.x = constrain(bird.x, 0, width-100);
+    console.log(bird.x); 
+    BackGroundColor= BackGroundColor+1;
+    //fill(bird.r, bird.g, bird.b);
+   // rect(width/2, height/2,rect_size);
+   // ellipse_size = ellipse_size-1;
+   // rect_size=rect_size+1;
 
 }
